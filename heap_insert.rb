9ebcfee7b0e26fa255d8caa3ddd1b28120b42b16
jamesbasa <jamesbasanmhs@gmail.com>
2023-05-08 18:20:24 -0700

    add heap insert file

---
+++ heap_insert.rb
@@ -0,0 +1,40 @@
+require './constants.rb'
+require './heap.rb'
+
+while true
+	arr = []
+	puts "Do you want to create a min or max heap?"
+
+	heap_type = gets.chomp.downcase
+	unless HEAP_TYPES.include? heap_type
+		puts "Please input either '#{MIN}' or '#{MAX}'"
+		puts
+		next
+	end
+
+	heap = Heap.new(arr, heap_type == MAX)
+
+	while true
+		puts "Input an integer to insert into the heap. Or input a non-integer to exit."
+
+		response = gets.chomp
+
+		begin
+			value = Integer(response)
+		rescue ArgumentError
+			puts "No integer detected. Exiting..."
+			break
+		end
+
+		unless value.is_a? Integer
+			puts "Please input a valid integer"
+			puts
+			next
+		end
+
+		heap.insert(value)
+		puts "The #{heap_type} heap: #{arr.inspect}"
+	end
+
+	break
+end
